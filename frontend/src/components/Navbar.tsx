952029a2fbf0fec7f8197787440d4f33670a80f2
Ruban <rubanaids@gmail.com>
2026-07-13 22:24:03 +0530

    Create Navbar.tsx

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+function Navbar() {
+  return (
+    <nav className="bg-blue-600 text-white shadow">
+
+      <div className="max-w-6xl mx-auto px-6 py-4">
+
+        <h1 className="text-2xl font-bold">
+          🌍 Air Quality Dashboard
+        </h1>
+
+      </div>
+
+    </nav>
+  );
+}
+
+export default Navbar;
